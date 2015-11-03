435c4a7a1a8382b63f4d7772793be45091726bff
Nicholas DIckey <nicholas.dickey@hudsonwilde.com>
2015-11-03 09:11:34 -0500

    added FB meta tags and dumping of all the thread comments for bots.

--- server.js
+++ server.js
@@ -359,13 +359,13 @@ function matchAndRender (err, payload,request,reply,ttl) {
 							<meta http-equiv="Pragma" content="no-cache" />
 							<meta http-equiv="Expires" content="0" />
 							<meta name="viewport" content="width=device-width, initial-scale=1 minimum-scale=0.5"/>
-							<meta name="description" content=${meta.description} />
-							<meta property="og:description" content=${meta.description} />
-							<meta property="og:title" content=${meta.title} />
-							<meta property="og:site_name" content=${meta.site_name} />
-							<meta property="og:url" content=${meta.url} />
-							<meta property="og:image" content=${meta.image} />
-							<meta property="og:locale" content=${meta.locale} />
+							<meta name="description" content=${'"'+meta.description+'"'} />
+							<meta property="og:description" content=${'"'+meta.description+'"'} />
+							<meta property="og:title" content=${'"'+meta.title+'"'} />
+							<meta property="og:site_name" content=${'"'+meta.site_name+'"'} />
+							<meta property="og:url" content=${'"'+meta.url+'"'} />
+							<meta property="og:image" content=${'"'+meta.image+'"'} />
+							<meta property="og:locale" content=${'"'+meta.locale+'"'} />
 							
 							
 
