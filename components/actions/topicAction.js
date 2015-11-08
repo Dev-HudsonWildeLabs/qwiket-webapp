9bc0515928aea8aa64bcc7b503fc7b4f4b7ed46f
Nicholas DIckey <nicholas.dickey@hudsonwilde.com>
2015-11-08 10:16:49 -0500

    wip - publish link

---
+++ components/actions/topicAction.js
@@ -0,0 +1,89 @@
+import 'babel-core/polyfill';
+import fetch from 'isomorphic-fetch';
+import Immutable from 'immutable';
+
+import * as common from '../d4shared/actions/common'
+
+export const START_SHARE_LINK = 'START_SHARE_LINK';
+export function startShareLink(url) {
+  return {
+    type: START_SHARE_LINK,
+    url
+  };
+}
+export const SUBMITTED_SHARE_LINK = 'SUBMITTED_SHARE_LINK';
+export function submittedShareLink(threadid) {
+  return {
+    type: SUBMITTED_SHARE_LINK,
+    threadid
+  };
+}
+export function selectCommunity(history,community){
+  
+}
+export function shareLink(link,state){
+  return function(dispatch) { //middleware thunk
+    if (state.isSubmitting) {
+      console.log('skipping shareLink')
+      return;
+    }
+    dispatch(startShareLink(url));
+    let url = `/api?task=submit_link&link=${link}&community=${community}`;
+    return fetch(url, {
+        credentials: 'same-origin'
+      })
+      .then(response => response.json())
+      .then(json => {
+        if (json.success) {
+          dispatch(submittedShareLink(json.threadid))
+          dispatch(loadTopic(json.threadid, state))
+        } else {
+          return dispatch(common.serviceError(json.msg))
+        }
+    });
+
+  }
+}
+export function loadTopic(threadid,state){
+  return function(dispatch) { //middleware thunk
+  dispatch(startShareLink());
+   let url = `/api?task=edit_thread&threadid=${threadid}`;
+    return fetch(url, {
+        credentials: 'same-origin'
+      })
+      .then(response => response.json())
+      .then(json => {
+        if (json.success) {
+          dispatch(receiveTopic(json.thread))
+        
+        } else {
+          return dispatch(common.serviceError(json.msg))
+        }
+    });
+  } 
+}
+export const RECEIVE_TOPIC = 'RECEIVE_TOPIC';
+export function receiveTopic(json) {
+  return {
+    type: RECEIVE_TOPIC,
+    thread:json
+  };
+}
+export function submitTopic(threadid,title,author,image,site_name,description,text,locale,community,state){
+  return function(dispatch) { //middleware thunk
+  dispatch(startShareLink());
+   let url = `/api?task=submit_topic&threadid=${threadid}&title=${encodeURIComponent(title)}&author=${encodeURIComponent(author)}&image=${encodeURIComponent(image)}&site_name=${site_name}&description=${encodeURIComponent(description)}&text=${encodeURIComponent(text)}&local${locale}&community=${community}`;
+    return fetch(url, {
+        credentials: 'same-origin'
+      })
+      .then(response => response.json())
+      .then(json => {
+        if (json.success) {
+          dispatch(submittedShareLink(json.thread))    
+        } else {
+          return dispatch(common.serviceError(json.msg))
+        }
+    });
+  } 
+}
+
