c32a690d5d0f589fad3d01204ade04426b80ec82
Nicholas DIckey <nicholas.dickey@hudsonwilde.com>
2015-11-14 18:42:44 -0500

    experementing with disqus forum

--- components/local.js
+++ components/local.js
@@ -4,28 +4,29 @@ var Local=React.createClass({
 	
 	
 	render: function(){
+    let topic=this.props.topic.toObject();
         //console.log('LOCAL RENDER %o',this.props)
-        if(this.props.topic.get("title")){
-		var t=this.props.topic.get("title").replace(/"/g,'\'');
-		t=t.replace(':','-');
-		let url="http://qwiket.com/"+this.props.community+"/"+this.props.topic.get("threadid")+"/7/";
-		//console.log('title=%s',t);
-		//console.log('shortname=%s',this.props.community)
-		//console.log('identifier=%s',this.props.topic.threadid)	
-		//console.log('url=%s',url)
-	   	return (
-            <ReactDisqusThread
-                shortname={this.props.community}
-                identifier={this.props.topic.get("threadid")}
-                title={t}
-                url={url}
-                categoryId="3710580"/>
-        );
-       }
-       else {
+    if(topic.title){
+  		var t=topic.title.replace(/"/g,'\'');
+  		t=t.replace(':','-');
+  		let url="http://qwiket.com/"+this.props.community+"/"+topic.threadid+"/7/";
+  		console.log('title=%s',t);
+  		console.log('shortname=%s',this.props.community)
+  		console.log('identifier=%s',topic.threadid)	
+  		console.log('url=%s',url)
+  	  return (
+              <ReactDisqusThread
+                  shortname={this.props.community}
+                  identifier={topic.threadid}
+                  title={t}
+                  url={url}
+                  categoryId="3710580"/>
+          );
+     }
+     else {
         return(
-            <div/>)
-       }
-	}
+          <div/>)
+     }
+  }
 });
 module.exports = Local;
