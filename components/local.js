c3f51078c9aab52cfff6189022b9aa2343e8006b
Nicholas DIckey <nicholas.dickey@hudsonwilde.com>
2015-11-14 19:03:47 -0500

    debug disqus

--- components/local.js
+++ components/local.js
@@ -2,14 +2,16 @@ import React from 'react'
 var ReactDisqusThread = require('react-disqus-thread');
 var Local=React.createClass({
 	
-	
+	 handleNewComment: function(comment) {
+        console.log(comment.text);
+    },
 	render: function(){
     let topic=this.props.topic.toObject();
         //console.log('LOCAL RENDER %o',this.props)
     if(topic.title){
   		var t=topic.title.replace(/"/g,'\'');
   		t=t.replace(':','-');
-  		let url="http://qwiket.com/"+this.props.community+"/"+topic.threadid+"/7/";
+  		let url="http://qwiket.com/context/"+this.props.community+"/topic/"+topic.threadid;
   		console.log('title=%s',t);
   		console.log('shortname=%s',this.props.community)
   		console.log('identifier=%s',topic.threadid)	
@@ -20,7 +22,9 @@ var Local=React.createClass({
                   identifier={topic.threadid}
                   title={t}
                   url={url}
-                  categoryId="3710580"/>
+                  categoryId="3710580"
+                  onNewComment={this.handleNewComment}/>
+
           );
      }
      else {
