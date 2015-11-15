b655822e78a1dfe6de00d42eef52f0786623abdf
Nicholas DIckey <nicholas.dickey@hudsonwilde.com>
2015-11-14 22:23:55 -0500

    dbg in prod

--- server.js
+++ server.js
@@ -543,9 +543,10 @@ function matchAndRender (err, payload,request,reply,ttl) {
 							<meta http-equiv="Pragma" content="no-cache" />
 							<meta http-equiv="Expires" content="0" />
 							<meta name="viewport" content="width=device-width, initial-scale=1 minimum-scale=0.5"/>
+							<meta property="og:title" content=${'"'+htmlEncode(meta.title)+'"'} />
 							<meta name="description" content=${'"'+htmlEncode(meta.description)+'"'} />
 							<meta property="og:description" content=${'"'+htmlEncode(meta.description)+'"'} />
-							<meta property="og:title" content=${'"'+htmlEncode(meta.title)+'"'} />
+							
 							<meta property="og:site_name" content=${'"'+htmlEncode(meta.site_name)+'"'} />
 							<meta property="og:url" content=${'"'+meta.url+'"'} />
 							<meta property="og:image" content=${'"'+meta.image+'"'} />
